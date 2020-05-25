e3a5815942e67dcbb9866fe1a4e8ecde1b26f387
Filip <Filip@DESKTOP-ERMMD1M>
2020-05-25 22:13:31 +0200

    #Proba za grane

---
+++ src/proba/Test2.java
@@ -0,0 +1,18 @@
+package proba;
+
+public class Test2 {
+	private int Filip;
+	private int Bojan;
+	public int getBojan() {
+		return Bojan;
+	}
+	public void setBojan(int bojan) {
+		Bojan = bojan;
+	}
+	public int getFilip() {
+		return Filip;
+	}
+	public void setFilip(int filip) {
+		Filip = filip;
+	}
+}
